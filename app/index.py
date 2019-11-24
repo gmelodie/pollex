a44b59d6ff02b0c9887fb91f8ea61e525c383783
Edylson Torikai <tedylson@gmail.com>
2019-11-24 11:10:07 -0300

    Add a simple POC for auth route API

---
+++ app/index.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/auth', methods=['POST'])
+def authenticate_user():
+  if 'username' in request.json and 'password' in request.json:
+
+  # TODO: verify user
+    return jsonify(id=42, token="$2fr43Pv"), 200
+  else:
+    return jsonify({ "error": "expected a json with username and password" }), 400
+
+app.run(debug=True)
